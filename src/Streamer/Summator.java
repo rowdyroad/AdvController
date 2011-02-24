8006c0cdeb660b42bf6032a4dd0abd610e4d27a5
unknown <Asus@.(none)>
2011-02-24 05:04:04 +0300

    Streamer.Summator: fix main algorithm

--- src/Streamer/Summator.java
+++ src/Streamer/Summator.java
@@ -1,22 +1,16 @@
 package Streamer;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import sun.security.util.Debug;
-
+import Calculation.MFCCComparer;
 import Common.Dbg;
 import Common.FingerPrint;
 import Common.Settings;
 import Common.Frequencier.Catcher;
-import Common.Utils;
 
 public class Summator implements Catcher, Loader.Processor {
 	
@@ -25,7 +19,6 @@ public class Summator implements Catcher, Loader.Processor {
 		float last = 0;
 		long maxed_time;
 		boolean has_max = false;
-		LinkedList<FrameWaiter> waiters = new LinkedList<FrameWaiter>();
 		FingerPrint fp;
 		public FingerPrintWrapper(FingerPrint fp)
 		{
@@ -39,50 +32,60 @@ public class Summator implements Catcher, Loader.Processor {
 	}
 
 	static int fw_id_ = 0;
-	
+	static float frame_equals_ = 0.1f;
 	private class FrameWaiter
 	{
-		long time;
 		long offset_begin;
 		long  offset_end;
 		long maxed_time;
-		long next_time;
 		float last = 0;
-		List<Float> lasts = new LinkedList<Float>();
 		int index;
 		float total;
 		long  timestamp;
-		boolean has_max = false;
 		int id;
-		int errors = Config.Instance().IgnoredErrors();
+		int errors = Config.Instance().IgnoredErrors(); 
 		float equip;
 		float total_equip;
+		boolean removed = false;
 		FingerPrintWrapper fpw;
 		
 		public FrameWaiter(FingerPrintWrapper fpw, long time, int index, float equip)
 		{
+			Set(fpw,time,index,equip);
+			//Utils.Dbg("%d, offsets:%d  %d (%d) %d  %d",off, this.offset.get(0),this.offset.get(1),this.offset.get(2),this.offset.get(3),this.offset.get(4));
+		}
+		
+		private long PrevTime(long time)
+		{
+			return (long) (time  + ( 1.0f - Config.Instance().OverlappedCoef())*  settings_.WindowSize());		
+		}
+		
+		private long NextTime(long time)
+		{
+			return (long) (time  + ( 1.0f + Config.Instance().OverlappedCoef())*  settings_.WindowSize());
+		}
+		
+		public void Set(FingerPrintWrapper fpw, long time, int index, float equip)
+		{
 			id=++fw_id_;
 			this.fpw = fpw;
-			this.time = time;
-			this.next_time = time + settings_.WindowSize();
-			this.offset_begin = (long) (time  + ( 1.0f - Config.Instance().OverlappedCoef()*2)*  settings_.WindowSize());
-			this.offset_end = (long) (time  + ( 1.0f + Config.Instance().OverlappedCoef()*2)*  settings_.WindowSize());
+			this.offset_begin = PrevTime(time);
+			this.offset_end = NextTime(time); 
 			this.index = index;
 			this.total = equip;
 			this.timestamp = System.currentTimeMillis();
 			this.equip =  equip;
 			this.total_equip = this.total / this.fpw.fp.Frames();
-			//Utils.Dbg("%d, offsets:%d  %d (%d) %d  %d",off, this.offset.get(0),this.offset.get(1),this.offset.get(2),this.offset.get(3),this.offset.get(4));
 		}
 		
 		public void Next(long time, float equip)
 		{
+			
 			++index;
 			this.total+=equip;
-			last  = 0;
-			this.offset_begin = (long) (time  + ( 1.0f - Config.Instance().OverlappedCoef()*2)*  settings_.WindowSize());
-			this.offset_end = (long) (time  + ( 1.0f + Config.Instance().OverlappedCoef()*2)*  settings_.WindowSize());
-			this.next_time = time + settings_.WindowSize();
+			this.last  = 0;
+			this.offset_begin = PrevTime(time);
+			this.offset_end = NextTime(time); 
 			this.equip = total / index;						
 			this.total_equip = this.total / this.fpw.fp.Frames();
 		}
@@ -93,15 +96,16 @@ public class Summator implements Catcher, Loader.Processor {
 	private DTW dtw_ ;
 	private Settings settings_ = null;
 	private long time_  =  0; 
-	private ArrayList<FrameWaiter> waiters_ = new ArrayList<FrameWaiter>();	
+	private List<FrameWaiter> waiters_ = new LinkedList<FrameWaiter>();	
 	private List<FingerPrintWrapper> fingerPrints_ = new CopyOnWriteArrayList<FingerPrintWrapper>();
 	private Resulter resulter_;
+	private List<FrameWaiter> removes_ = new LinkedList<FrameWaiter>();
 	
 	public Summator(Settings settings, Resulter resulter)
 	{
 		settings_ = settings;
 		resulter_ = resulter;
-		dtw_  = new DTW((int) (2.0f / Config.Instance().OverlappedCoef() - 1));
+		dtw_  = new DTW((int) (2.0f / Config.Instance().OverlappedCoef() - 1));		
 	}
 
 	@Override
@@ -116,28 +120,6 @@ public class Summator implements Catcher, Loader.Processor {
 		fingerPrints_.remove(new FingerPrintWrapper(fp));
 	} 
 	
-	private List<FrameWaiter> removes_ = new LinkedList<FrameWaiter>();
-	
-	private void DbgMFCC(float[][] mfcc)
-	{
-		String str = new String();
-					
-			for (int j = 0; j < mfcc.length; ++ j)
-			{
-				str+="";
-				for (int k = 0; k <  mfcc[j].length; ++k)
-				{
-					str+=String.format("%.03f\t",  mfcc[j][k]);
-				}
-				str+="\n";
-			}
-			
-		Dbg.Debug(str);
-	}
-	
-	float[][] last_mfcc;	
-	
-	FrameWaiter max_fw = null;
 	
 	private void  submitResult(FrameWaiter fw)
 	{
@@ -145,37 +127,57 @@ public class Summator implements Catcher, Loader.Processor {
 		fw.Next(fw.maxed_time, fw.last);				
 		if (fw.index >= fw.fpw.fp.Frames() *  Config.Instance().Equals())
 		{
-			Dbg.Info("!!! res[%d]: %s[%d]  %f",fw.index,fw.fpw.fp.Id(),fw.id, fw.total_equip);			
+			Dbg.Debug("!!! res: %s[%d][%d]  %f",fw.fpw.fp.Id(),fw.id, fw.index, fw.total_equip);			
 			resulter_.OnFound(fw.fpw.fp.Id(), fw.timestamp, System.currentTimeMillis(),  fw.total_equip);						
-				for (int i = 0;  i < waiters_.size(); ++i)
-				{
-					final FrameWaiter fwz = waiters_.get(i);
+				
+				Iterator<FrameWaiter> it = waiters_.iterator();
+				while (it.hasNext())
+				{				
+					final FrameWaiter fwz = it.next();
 					if (fwz.equip < fw.equip)
 					{
+						fwz.removed = true;
 						removes_.add(fwz);
 					}
 				}
+				Dbg.Debug("rem: %s[%d][%d] - %f",fw.fpw.fp.Id(), fw.id, fw.index, fw.fpw.last);
+				fw.removed = true;
 				removes_.add(fw);			
 		}
 	}
-		
+	
+	private void DbgMFCC(float[][] mfcc)
+	{
+		String str = "";
+		for (int j = 0; j < mfcc.length; ++ j)
+		{
+			str+="";
+			for (int k = 0; k <  mfcc[j].length; ++k)
+			{
+				str+=String.format("%.03f\t",  mfcc[j][k]);
+			}
+			str+="\n";
+		}		
+		Dbg.Debug(str);
+	}
+	
 	public boolean OnReceived(float[][] mfcc, long timeoffset) 
 	{		
-		int news = 0;
+		int exists =  waiters_.size();
+		Collections.shuffle(fingerPrints_);	
 		for (FingerPrintWrapper fpw: fingerPrints_)
 		{
-			final float x = dtw_.measure(fpw.fp.Get(0), mfcc);
-			if (x > 0.1f)
+			final float x = dtw_.measure(fpw.fp.Get(0), mfcc);	
+			if (x > frame_equals_)
 			{
 					if (x < fpw.last)
 					{
 							if (fpw.has_max)
 							{
-								Dbg.Debug("new: %s - %f/%f %d %d",fpw.fp.Id(), fpw.last, x, fpw.maxed_time, time_);
 								fpw.has_max = false;
-								FrameWaiter fw = new FrameWaiter(fpw, fpw.maxed_time, 1,fpw.last);			
+								FrameWaiter fw = new FrameWaiter(fpw, fpw.maxed_time, 1,fpw.last);																
+								Dbg.Debug("new: %s[%d] - %f/%f %d %d",fpw.fp.Id(),fw.id, fpw.last, x, fpw.maxed_time, time_);
 								waiters_.add(fw);
-								news++;
 							}
 					}
 					else
@@ -186,25 +188,71 @@ public class Summator implements Catcher, Loader.Processor {
 					fpw.last = x;
 			}
 		}
-		removes_.clear();	
-		for (int i = 0; i < waiters_.size() - news; ++i)
+				
+		Iterator<FrameWaiter> it = waiters_.iterator();
+		int i = 0;
+		while (it.hasNext())
 		{
-			FrameWaiter fw = waiters_.get(i);
-			if (time_ < fw.offset_begin)
+			if (i++ >= exists) break;
+			
+			FrameWaiter fw = it.next();				
+			if (fw.removed || time_ < fw.offset_begin)
 			{
 				continue;
 			}
 			
 			if (time_ > fw.offset_end)
 			{
-				if (fw.has_max && fw.last > 0.1f)
+					if (fw.last > frame_equals_)
+					{
+						submitResult(fw);
+					}
+					else
+					{
+						Dbg.Debug("rem: %s[%d][%d] - %f [%d  - %d]",fw.fpw.fp.Id(), fw.id, fw.index,  fw.last, fw.offset_begin, fw.offset_end);
+						fw.removed = true;
+						removes_.add(fw);									
+					}
+					continue;
+			}
+			
+			final float x = dtw_.measure(fw.fpw.fp.Get(fw.index), mfcc);
+			
+			
+			if (x > frame_equals_ )
+			{				
+				if (x > fw.last)
+				{
+					fw.last = x;
+					fw.maxed_time = time_;
+				}
+			}
+			else
+			{
+					if (--fw.errors < 0)
+					{
+						Dbg.Debug("rem: %s[%d][%d] - %f/%f",fw.fpw.fp.Id(), fw.id, fw.index,  fw.last, x);
+						fw.removed = true;
+						removes_.add(fw);
+					}
+			}
+			
+			/*if (time_ > fw.offset_end)
+			{
+				if (fw.last > 0.1f)
 				{
 					submitResult(fw);
 				}
 				else
 				{
-					waiters_.remove(fw);
-					--i;
+					if (fw.errors <= 0)
+					{
+						Dbg.Debug("rem: %s[%d][%d] - %f",fw.fpw.fp.Id(), fw.id, fw.index, fw.last);
+						removes_.add(fw);
+						continue;
+					}							
+					Dbg.Debug("err: %s[%d][%d] - %f %d",fw.fpw.fp.Id(), fw.id, fw.index, fw.last, fw.errors);
+					--fw.errors;							
 				}
 				continue;
 			}
@@ -218,13 +266,20 @@ public class Summator implements Catcher, Loader.Processor {
 						if (fw.last > 0.1f)
 						{							
 							submitResult(fw);
-							fw.has_max = false;
 						}
 						else
 						{
-							removes_.add(fw);
-							continue;
-						}						
+							if (fw.errors <= 0)
+							{
+								Dbg.Debug("rem: %s[%d][%d] - %f %f",fw.fpw.fp.Id(), fw.id, fw.index, fw.last,x);
+								removes_.add(fw);
+								continue;
+							}			
+							Dbg.Debug("err: %s[%d][%d] - %f %d",fw.fpw.fp.Id(), fw.id, fw.index, fw.last, fw.errors);
+							--fw.errors;							
+						}
+						
+						fw.has_max = false;						
 				}
 			}
 			else
@@ -232,11 +287,18 @@ public class Summator implements Catcher, Loader.Processor {
 				fw.maxed_time =  time_;
 				fw.has_max = true;
 			}
+			fw.last = x;*/
 			
-			fw.last = x;
+			
 		}
+			
+			
 	
-		
+		waiters_.removeAll(removes_);
+		Collections.shuffle(waiters_);
+		removes_.clear();
+		time_+=timeoffset;
+		return true;
 	//	Dbg.Info("%f",total_ / totals_);
 	/*	FingerPrintWrapper max_fpw = null;
 		float max = 0;
@@ -331,10 +393,7 @@ public class Summator implements Catcher, Loader.Processor {
 					}
 			}
 		}		*/
-		waiters_.removeAll(removes_);
-		removes_.clear();
-		time_+=timeoffset;
-		return true;
+
 	}
 
 	@Override
