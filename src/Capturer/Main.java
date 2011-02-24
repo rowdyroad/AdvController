559a2bf0a624a0b298ff5cc8f9b72b83bd31e379
unknown <Asus@.(none)>
2011-02-24 04:51:46 +0300

    Capturer.Main: add loading wav files from directory support

--- src/Capturer/Main.java
+++ src/Capturer/Main.java
@@ -1,6 +1,11 @@
 package Capturer;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -16,6 +21,7 @@ public class Main {
 		Dbg.Info("Usage: %s\n-c <config_file>\n-C <channel>\n-f <min_frequency>\n-F <max_frequency> \n-p <wav_file>,<result_file>,<promo_id>,...\n-b <buffer_count>", Main.class.getName());
 		System.exit(1);		
 	}
+	
 	
 	public static void main (String  args[])  {
 
@@ -37,7 +43,40 @@ public class Main {
 			usage();
 		}
 	
-		String[]  promos = promo.split(",");
+		String[]  promos = promo.split(",");		
+		if (promos.length == 2)
+		{
+			File src_dir = new File(promos[0]);
+			File dst_dir = new File(promos[1]);
+			
+			if (!src_dir.isDirectory() || !dst_dir.isDirectory())
+			{
+				usage();
+			}
+			
+			Map<String,String> map = new TreeMap<String,String>();
+			
+			String[] src_files = src_dir.list();			
+			for (String src_file : src_files)
+			{
+				if (! src_file.endsWith(".wav")) continue;				
+				map.put(src_file.substring(0, src_file.length() - 4), src_file);				
+			}
+			
+			promos = new String[map.size() * 3];
+			int i = 0;
+			for (Entry<String,String> kvp : map.entrySet())
+			{
+				promos[i] = src_dir.getPath()+File.separator + kvp.getValue();
+				promos[i+1] = dst_dir.getPath() + File.separator + kvp.getKey();
+				promos[i+2] = kvp.getKey();
+				i+=3;
+			}
+		}
+		
+		
+		
+		
 		if (promos.length % 3 != 0)
 		{
 			usage();
@@ -62,6 +101,7 @@ public class Main {
 				capt.Process().Serialize(result_file);
 				Dbg.Info("Time to work: %d ms", System.currentTimeMillis() - time);
 			}
+			
 			System.exit(0);
 		}
 		catch (Source.SourceException e)
